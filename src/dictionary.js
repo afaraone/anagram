1851a56c04d2fd4ea8a3689f9f9986e49a2a8ae6
afaraone <andresfaraone@outlook.com>
2018-12-07 16:23:18 +0000

    add createDictionary
    converts an array of strings into hash
    where key is sortedWord and val is array of words
    
    this can then be used to look up anagrams

--- src/dictionary.js
+++ src/dictionary.js
@@ -1,7 +1,21 @@
 // creates an anagram dictionary
+const {sortWord} = require('./sorter');
 
 const parseText = text => {
   return text.split('\n')
 };
 
-module.exports = {parseText}
+const createDictionary = wordList => {
+  let dictionary = {};
+  for (let i = 0; i < wordList.length; i++) {
+    let word = wordList[i];
+    let sortedWord = sortWord(word);
+    if (dictionary[sortedWord] === undefined) {
+      dictionary[sortedWord] = [];
+    };
+    dictionary[sortedWord].push(word);
+  };
+  return dictionary;
+};
+
+module.exports = {parseText, createDictionary}
--- test/dictionary.js
+++ test/dictionary.js
@@ -1,11 +1,18 @@
 const expect = require('chai').expect;
 const {parseText} = require('../src/dictionary');
+const {createDictionary} = require('../src/dictionary');
 
 const text = "dog\ngod\nopt\ntop";
 const wordArray = ["dog", "god", "opt", "top"];
-
+const dictionary = {dgo: ["dog", "god"], opt: ["opt", "top"]};
 describe('parseText', () => {
   it('returns an array of words', () => {
     expect(parseText(text)).to.deep.equal(wordArray);
+  });
+});
+
+describe('createDictionary', () => {
+  it('returns hash, key is sorted word val is array of words', () => {
+    expect(createDictionary(wordArray)).to.deep.equal(dictionary);
   });
 });
